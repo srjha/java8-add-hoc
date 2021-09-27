c2e59375270f524e46dbde8465b0621e0b8fda98
Shashi Ranjan <shashi2587@gmail.com>
2021-09-27 11:50:37 +0200

    Add files via upload

---
+++ src/main/java/txt/TxtProc.java
@@ -0,0 +1,11 @@
+package txt;
+
+public class TxtProc {
+
+    public static void main(String[] args) {
+        String cities = "Stockholm,Göteborg,Malmö,Helsingborg";
+
+        // Parse into a list
+
+    }
+}
